2d09ba2c5ed60d4997e1783c22bc7c00d22bab3e
lXSPandora <luizepauloxd@gmail.com>
2018-07-15 22:24:01 -0300

    ✨ Adds new assets files

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export const Images = {
+  logo: require('./nikelogo.png'),
+  vapor: require('./vapormax.png'),
+  vaporMax1: require('./vapormax2.png'),
+  vaporMAx2: require('./vapormax3.png'),
+}
